fefbd69c97c63cf449306956a8f0af01559bec9a
Alan Engelhardt <ale@kea.dk>
2017-03-19 20:09:49 +0100

    day 1+ examples

---
+++ t.js
@@ -0,0 +1,16 @@
+function trans(lang) {
+	if (lang == 'dk') {
+		console.log('goddag')
+	}
+	else {
+		console.log('hello')
+	}
+}
+
+let arr = [1, 2, 3, 8, 10]
+
+arr.forEach(listArr)
+
+function listArr(item){
+	console.log(item)
+}
